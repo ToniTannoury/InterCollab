207253c2dbc458c4dd99ab70a09b891a9ce07ebb
ToniTannoury <tatannoury01@gmail.com>
2023-09-22 02:33:30 +0300

    Initiated followings state

--- frontend/src/app/followings/page.tsx
+++ frontend/src/app/followings/page.tsx
@@ -1,7 +1,19 @@
 'use client'
 import SearchUserInput from '@/components/SearchUserInput'
 import React, { use, useEffect, useState } from 'react'
-
+import Cookies from 'js-cookie';
+import { useDispatch, useSelector } from 'react-redux'
+import { setLoading } from '@/redux/loadersSlice'
+import { message } from 'antd';
+import Image from 'next/image';
+import { removeFollowing } from '@/redux/usersSlice';
+interface MyObject {
+  _id: string;
+  name: string;
+  email: string;
+  password: string;
+  followings: any[]; 
+}
 function page() {
 
 
