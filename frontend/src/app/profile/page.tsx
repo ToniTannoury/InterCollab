21169023179e23eeaf02894c67d2e312feb941c2
ToniTannoury <tatannoury01@gmail.com>
2023-09-22 01:06:09 +0300

    Removed room info page

--- frontend/src/app/profile/page.tsx
+++ frontend/src/app/profile/page.tsx
@@ -9,76 +9,13 @@ import { useRouter } from 'next/navigation'
 import UserInfoBar from '@/components/UserInfoBar'
 import Cookies from 'js-cookie'
 function Pofile() {
-  const {currentUser} = useSelector((state:any)=>state.users) 
-  const dispatch = useDispatch()
-  const router = useRouter()
- 
 
-  const onFinish = async (values:any)=>{
-    try {
-      values._id = currentUser._id
-      values.userType = currentUser.userType
-      dispatch(setLoading(true))
-      const response = await fetch("http://16.171.116.7:5000/api/users/updateUser", {
-        method: 'PUT',
-        headers: {
-          'Content-Type': 'application/json',
-          "Authorization": `Bearer ${Cookies.get('token')}`
-        },
-        body:JSON.stringify(values)
-      });
-      message.success("Profile updated successfully")
-      const data = await response.json()
 
-      dispatch(setCurrentUser(data.data))
-    } catch (error:any) {
-      message.error(error.message.data.message || "Something went wrong")
-    }finally{
-      dispatch(setLoading(false))
-    }
-  }
+
   return (
     <div>
-      <h1 className='text-4xl mb-3 month-header text-ICblue'>Profile</h1>
     
-      <UserInfoBar user={currentUser}/>
-    {currentUser &&<Form layout='vertical' initialValues={currentUser} onFinish={onFinish}>
-      <>
-      <Row gutter={[16,16]}>
-        
-        <Col span={8}>
-          <Form.Item label='Name' name="name" >
-            <input className='input' type="text" required/>
-          </Form.Item>
-        </Col>
-        <Col span={8}>
-          <Form.Item label='Email' name="email" >
-            <input className='input' type="text" required/>
-          </Form.Item>
-        </Col>
-        <Col span={8}>
-          <Form.Item label='Phone' name="phone" >
-            <input className='input ' type="text" required/>
-          </Form.Item>
-        </Col>
-        <Col span={24}>
-          <Form.Item label='About' name="about" >
-            <textarea className='input text-area' required/>
-          </Form.Item>
-        </Col>
-   
-      
-      </Row>
-      
-      
-    </>
-        <div className='flex justify-end my-3'>
     
-          <Button className='bg-ICblue h-10' type="primary" htmlType='submit'>
-            Save
-          </Button>
-        </div>
-      </Form>}
     </div>
   )
 }
--- frontend/src/app/roomInfo/[roomId]/page.tsx
+++ frontend/src/app/roomInfo/[roomId]/page.tsx
@@ -1,164 +1,164 @@
-'use client'
+// 'use client'
 
-import { setLoading } from "@/redux/loadersSlice"
-import { Button, Col, Divider, Row, message } from "antd"
-import { useParams, useRouter } from "next/navigation"
-import { useContext, useEffect, useState } from "react"
-import { useDispatch, useSelector } from 'react-redux'
-import Cookies from "js-cookie"
-import { setCurrentUser } from "@/redux/usersSlice"
-import { RoomContext } from "@/context/RoomContext"
-// import socketIO from 'socket.io-client'
-// const WS = 'http://16.171.116.7:5000'
-function RoomInfo() {
-  const router = useRouter()
-  const {roomId} = useParams()
-  const dispatch = useDispatch()
-  const [roomData , setRoomData] = useState<any>(null)
-  const [code , setCode] = useState<string>('')
-  const {currentUser } = useSelector((state:any)=>state.users)
+// import { setLoading } from "@/redux/loadersSlice"
+// import { Button, Col, Divider, Row, message } from "antd"
+// import { useParams, useRouter } from "next/navigation"
+// import { useContext, useEffect, useState } from "react"
+// import { useDispatch, useSelector } from 'react-redux'
+// import Cookies from "js-cookie"
+// import { setCurrentUser } from "@/redux/usersSlice"
+// import { RoomContext } from "@/context/RoomContext"
+// // import socketIO from 'socket.io-client'
+// // const WS = 'http://16.171.116.7:5000'
+// function RoomInfo() {
+//   const router = useRouter()
+//   const {roomId} = useParams()
+//   const dispatch = useDispatch()
+//   const [roomData , setRoomData] = useState<any>(null)
+//   const [code , setCode] = useState<string>('')
+//   const {currentUser } = useSelector((state:any)=>state.users)
 
-  const {ws , me , stream ,peers,shareScreen , participants ,setRoomId,screenSharringId , messages, mediaShareStatus, setMediaShareStatus , leaveRoom , setMessages ,removeAllPeers } = useContext(RoomContext)
-  const fetchJob = async()=>{
-    try {
-      dispatch(setLoading(true))
-      const response = await fetch(`http://16.171.116.7:5000/api/rooms/searchRoomById?roomId=${roomId}`,{
-        headers:{
-          "Authorization":`Bearer ${Cookies.get('token')}`
-        }
-      })
-      const data = await response.json()
-      setRoomData(data)
-    } catch (error:any) {
-      message.error(error.message)
-    }finally{
-      dispatch(setLoading(false))
-    }
-  }
+//   const {ws , me , stream ,peers,shareScreen , participants ,setRoomId,screenSharringId , messages, mediaShareStatus, setMediaShareStatus , leaveRoom , setMessages ,removeAllPeers } = useContext(RoomContext)
+//   const fetchJob = async()=>{
+//     try {
+//       dispatch(setLoading(true))
+//       const response = await fetch(`http://16.171.116.7:5000/api/rooms/searchRoomById?roomId=${roomId}`,{
+//         headers:{
+//           "Authorization":`Bearer ${Cookies.get('token')}`
+//         }
+//       })
+//       const data = await response.json()
+//       setRoomData(data)
+//     } catch (error:any) {
+//       message.error(error.message)
+//     }finally{
+//       dispatch(setLoading(false))
+//     }
+//   }
 
-  const coinTransfer = async ()=>{
-    if(currentUser.coins<roomData.priceToEnter){
-      message.error("Not enough coins to  join")
-      return
-    }
-    router.push(`/test/${roomData._id}`)
-    dispatch(setCurrentUser({
-      ...currentUser,
-      coins:currentUser.coins-roomData.priceToEnter
-    }))
-      const response = await fetch(`http://16.171.116.7:5000/api/users/coinTransfer`,{
-      method:"POST",
-        headers:{
-          "Content-Type":"application/json",
-          "Authorization":`Bearer ${Cookies.get('token')}`
-        },
-        body:JSON.stringify({
-          roomId:roomId,
-          amount:roomData.priceToEnter
-        })
-      })
-      const data = await response.json() 
-  }
-  useEffect(()=>{
-    fetchJob()
-  },[])
-  useEffect(()=>{
-  },[currentUser])
-  const checkPin = async(e:any)=>{
-    e.preventDefault()
-    roomData.pinCode === code && router.push(`/test/${roomData._id}`)
-  }
-  const joinNow = async ()=>{  
-    router.push(`/test/${roomData._id}`)
-  }
+//   const coinTransfer = async ()=>{
+//     if(currentUser.coins<roomData.priceToEnter){
+//       message.error("Not enough coins to  join")
+//       return
+//     }
+//     router.push(`/test/${roomData._id}`)
+//     dispatch(setCurrentUser({
+//       ...currentUser,
+//       coins:currentUser.coins-roomData.priceToEnter
+//     }))
+//       const response = await fetch(`http://16.171.116.7:5000/api/users/coinTransfer`,{
+//       method:"POST",
+//         headers:{
+//           "Content-Type":"application/json",
+//           "Authorization":`Bearer ${Cookies.get('token')}`
+//         },
+//         body:JSON.stringify({
+//           roomId:roomId,
+//           amount:roomData.priceToEnter
+//         })
+//       })
+//       const data = await response.json() 
+//   }
+//   useEffect(()=>{
+//     fetchJob()
+//   },[])
+//   useEffect(()=>{
+//   },[currentUser])
+//   const checkPin = async(e:any)=>{
+//     e.preventDefault()
+//     roomData.pinCode === code && router.push(`/test/${roomData._id}`)
+//   }
+//   const joinNow = async ()=>{  
+//     router.push(`/test/${roomData._id}`)
+//   }
   
-  return (
-    roomData && <div>
-      <h1 className="text-2xl -mt-1 mb-3">{roomData.title}</h1>
-      <Row gutter={[16,16]}>
-        <Col span={12} className="flex flex-col gap-2">
-          <div className="flex justify-between">
-            <span>
-              Creator's name
-            </span>
-            <span>
-              {roomData.user?.name}
-            </span>
-          </div>
-          <div className="flex justify-between">
-            <span>
-            Creator's rating
-            </span>
-            <span>
-              {roomData.user?.rating}
-            </span>
-          </div>
-          <div className="flex justify-between">
-            <span>
-            Creator's email
-            </span>
-            <span>
-              {roomData.user?.email}
-            </span>
-          </div>
-          <div className="flex justify-between">
-            <span>
-            Room Category
-            </span>
-            <span>
-              {roomData.category}
-            </span>
-          </div>
-          <div className="flex justify-between">
-            <span>
-            Max number of participants
-            </span>
-            <span>
-              {roomData.maxNumberOfParticipants}
-            </span>
-          </div>
-          <div className="flex justify-between">
-            <span>
-              Room Type
-            </span>
-            <span>
-              {roomData.type}
-            </span>
-          </div>
-        </Col>
-        <Col span={24} className="flex flex-col gap-2">
-          <h1 className="text-xl">
-            About the creator
-          </h1>
-          <Divider className="p-0 h-0 m-0"/>
-          <span>{roomData.user?.about}</span>
-          <h1 className="text-xl">
-            Room Description
-          </h1>
-          <Divider className="p-0 h-0 m-0"/>
-          <span>{roomData.description}</span>
+//   return (
+//     roomData && <div>
+//       <h1 className="text-2xl -mt-1 mb-3">{roomData.title}</h1>
+//       <Row gutter={[16,16]}>
+//         <Col span={12} className="flex flex-col gap-2">
+//           <div className="flex justify-between">
+//             <span>
+//               Creator's name
+//             </span>
+//             <span>
+//               {roomData.user?.name}
+//             </span>
+//           </div>
+//           <div className="flex justify-between">
+//             <span>
+//             Creator's rating
+//             </span>
+//             <span>
+//               {roomData.user?.rating}
+//             </span>
+//           </div>
+//           <div className="flex justify-between">
+//             <span>
+//             Creator's email
+//             </span>
+//             <span>
+//               {roomData.user?.email}
+//             </span>
+//           </div>
+//           <div className="flex justify-between">
+//             <span>
+//             Room Category
+//             </span>
+//             <span>
+//               {roomData.category}
+//             </span>
+//           </div>
+//           <div className="flex justify-between">
+//             <span>
+//             Max number of participants
+//             </span>
+//             <span>
+//               {roomData.maxNumberOfParticipants}
+//             </span>
+//           </div>
+//           <div className="flex justify-between">
+//             <span>
+//               Room Type
+//             </span>
+//             <span>
+//               {roomData.type}
+//             </span>
+//           </div>
+//         </Col>
+//         <Col span={24} className="flex flex-col gap-2">
+//           <h1 className="text-xl">
+//             About the creator
+//           </h1>
+//           <Divider className="p-0 h-0 m-0"/>
+//           <span>{roomData.user?.about}</span>
+//           <h1 className="text-xl">
+//             Room Description
+//           </h1>
+//           <Divider className="p-0 h-0 m-0"/>
+//           <span>{roomData.description}</span>
          
-          <div className="flex justify-end gap-3">
+//           <div className="flex justify-end gap-3">
           
-          <Button type="default" onClick={()=>router.back()}>Cancel</Button>
+//           <Button type="default" onClick={()=>router.back()}>Cancel</Button>
 
-          {(roomData.type === "public" || roomData.user._id === currentUser._id)&&roomData.type !== "paid" && <Button type="primary" className="bg-ICblue"  onClick={joinNow}>Join Now</Button>}
+//           {(roomData.type === "public" || roomData.user._id === currentUser._id)&&roomData.type !== "paid" && <Button type="primary" className="bg-ICblue"  onClick={joinNow}>Join Now</Button>}
 
-          {roomData.type === "private" && roomData.user._id !== currentUser._id &&
-          (
-            <form onSubmit={checkPin} action="">
-              <input value={code} className="pincode" type="text" onChange={(e)=>setCode(e.target.value)}/>
-            </form>
-          )}
-           {(roomData.type === "paid" )&&roomData.type !== "public" &&  roomData.user._id !== currentUser._id&&<Button onClick={coinTransfer} type="primary" className="bg-ICblue" >Join Now For {roomData.priceToEnter} Coins</Button>}
-           {(roomData.type === "paid" )&&roomData.type !== "public" &&  roomData.user._id === currentUser._id&&<Button type="primary" className="bg-ICblue"  onClick={joinNow}>Join Now</Button>}
-        </div>
+//           {roomData.type === "private" && roomData.user._id !== currentUser._id &&
+//           (
+//             <form onSubmit={checkPin} action="">
+//               <input value={code} className="pincode" type="text" onChange={(e)=>setCode(e.target.value)}/>
+//             </form>
+//           )}
+//            {(roomData.type === "paid" )&&roomData.type !== "public" &&  roomData.user._id !== currentUser._id&&<Button onClick={coinTransfer} type="primary" className="bg-ICblue" >Join Now For {roomData.priceToEnter} Coins</Button>}
+//            {(roomData.type === "paid" )&&roomData.type !== "public" &&  roomData.user._id === currentUser._id&&<Button type="primary" className="bg-ICblue"  onClick={joinNow}>Join Now</Button>}
+//         </div>
        
-        </Col>
+//         </Col>
         
-      </Row>
-    </div>
-  )
-}
+//       </Row>
+//     </div>
+//   )
+// }
 
-export default RoomInfo
+// export default RoomInfo
