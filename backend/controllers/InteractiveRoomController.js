2fdb3dcc29953323004fa4c993682b63e6584664
ToniTannoury <tatannoury01@gmail.com>
2023-09-21 03:03:56 +0300

    Implemented leave  room socket emition

--- backend/controllers/InteractiveRoomController.js
+++ backend/controllers/InteractiveRoomController.js
@@ -6,6 +6,30 @@ const roomHandler = (socket) => {
   const createRoom = ({ roomId }) => {
     socket.emit('room-created', { roomId });
   };
+  const leaveRoom = async ({ roomId, peerId }) => {
+    try {
+      console.log("User disconnected");
+      socket.to(roomId).emit("user-disconnected", peerId);
+  
+      const objectId1 = new mongoose.Types.ObjectId(peerId);
+  
+      const updatedRoom = await Room.findOneAndUpdate(
+        { _id: roomId },
+        {
+          $pull: { currentParticipants: { _id: objectId1 } },
+        },
+        { new: true }
+      );
+  
+      if (!updatedRoom) {
+        console.error("Room not found.");
+        return;
+      }
+  
+    } catch (error) {
+      console.error("Error leaving room:", error);
+    }
+  };
 };
 
 module.exports = roomHandler;
