f82bfe4d110f56ff4b813d2c55062fc19319b2cc
ToniTannoury <tatannoury01@gmail.com>
2023-09-22 01:13:25 +0300

    initiated create new room page

---
+++ frontend/src/app/createRoom/page.tsx
@@ -0,0 +1,31 @@
+"use client"
+import React, { useContext, useEffect, useState } from 'react';
+import { Button, Form, Col, Row, message } from 'antd';
+import { useDispatch, useSelector } from 'react-redux';
+import { setLoading } from '@/redux/loadersSlice';
+import Cookies from 'js-cookie';
+import socketIO from 'socket.io-client';
+import { useRouter } from 'next/navigation';
+import { RoomContext } from '@/context/RoomContext';
+
+function CreateRoom() {
+  const { currentUser } = useSelector((state: any) => state.users);
+  const dispatch = useDispatch();
+  const router = useRouter();
+  const { ws } = useContext(RoomContext);
+  const [showCoinInput, setShowCoinInput] = useState(false); // Add state for showing coin input
+
+
+
+  const onFinish = async (values: any) => {
+   
+  };
+
+
+
+  return (
+    
+  );
+}
+
+export default CreateRoom;
