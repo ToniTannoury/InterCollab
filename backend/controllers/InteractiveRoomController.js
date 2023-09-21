00ceaa2a234e0c8f9fefa34863185d442d937c1a
ToniTannoury <tatannoury01@gmail.com>
2023-09-21 03:03:24 +0300

    Implemented create room socket emition

--- backend/controllers/InteractiveRoomController.js
+++ backend/controllers/InteractiveRoomController.js
@@ -3,7 +3,9 @@ const User = require('../models/userModel');
 const mongoose = require('mongoose')
 
 const roomHandler = (socket) => {
-
+  const createRoom = ({ roomId }) => {
+    socket.emit('room-created', { roomId });
+  };
 };
 
 module.exports = roomHandler;
