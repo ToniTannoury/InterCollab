e082cb9586619e126737907c6cbdb912f319e55a
ToniTannoury <tatannoury01@gmail.com>
2023-09-23 02:05:55 +0300

    Instantiated context state

--- frontend/src/context/RoomContext.tsx
+++ frontend/src/context/RoomContext.tsx
@@ -19,7 +19,23 @@ const ws = socketIO(WS);
 export const RoomProvider: React.FunctionComponent<RoomProviderProps> = ({
   children,
 }) => {
-nst dispatching =  useDispatch()
+  const [screenSharringId , setScreenSharringId] = useState<boolean>(false)
+  const path = usePathname()
+  const {currentUser} = useSelector((state:any)=>state.users)
+  const [me , setMe] = useState<Peer>()
+  const [stream , setStream] = useState<MediaStream>()
+  const [peers , dispatch] = useReducer(peersReducer , {})
+  const [participants , setParticipants] = useState<any>([])
+  const [isRating, setIsRating] = useState<boolean>(false);
+  const [connectionArray, setConnectionArray] = useState<any>([]);
+  const [roomId , setRoomId] = useState<any>("")
+  const [messages,setMessages] = useState<any>([]) 
+  const [mediaShareStatus, setMediaShareStatus] = useState<boolean>(true);
+  const memoizedStream = useMemo(() => stream, [stream]);
+  const [isRoomInfoModalOpen , setIsRoomInfoModalOpen] = useState<boolean>(false)
+  const [chosenRoom , setChosenRoom] = useState<string>('')
+ const router = useRouter()
+  const dispatching =  useDispatch()
 
 
 
