42f5608fd8c485b426e0555eaea8e760bb61eaf2
ToniTannoury <tatannoury01@gmail.com>
2023-09-23 02:21:04 +0300

    Created handle follow click

--- frontend/src/components/UserInfoBar.tsx
+++ frontend/src/components/UserInfoBar.tsx
@@ -19,7 +19,39 @@ const UserInfoBar = ({ user, search }: any) => {
     setIsFollowing(currentUser?.followings?.some((followingUser: any) => followingUser === user?._id));
   }, [user?.followings, currentUser?.followings, user?._id]);
 
+  const handleFollowClick = async () => {
+    try {
+      const url = isFollowing
+        ? `http://16.171.116.7:5000/api/users/unfollow/${user._id}`
+        : `http://16.171.116.7:5000/api/users/follow/${user._id}`;
 
+      const response = await fetch(url, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+          "Authorization": `Bearer ${Cookies.get('token')}`
+        }
+      });
+
+      if (response.ok) {
+        setIsFollowing(!isFollowing);
+
+        setFollowerCount((prevCount:number )=> isFollowing ? prevCount - 1 : prevCount + 1);
+
+        if (isFollowing) {
+          dispatch(removeFollowing(user._id));
+        } else {
+          dispatch(addFollowing(user));
+        }
+      } else {
+        console.error('Error:', response.statusText);
+      }
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+
+  const userId = user?._id;
 
 
   
