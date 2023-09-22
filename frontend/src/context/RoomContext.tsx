09fbf0dce905d990a1b3931fc7f35b1f6e300b30
ToniTannoury <tatannoury01@gmail.com>
2023-09-23 02:04:47 +0300

    Initiated room context

--- frontend/src/context/RoomContext.tsx
+++ frontend/src/context/RoomContext.tsx
@@ -1,1 +1,5 @@
 'use client'
+import socketIO from "socket.io-client";
+import { createContext, ReactNode, useEffect, useState , useReducer, useMemo} from "react";
+import { useDispatch, useSelector } from "react-redux";
+import Peer from "peerjs";
