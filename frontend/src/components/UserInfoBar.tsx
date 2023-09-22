dfeafcc43a5bcdde64cafd3a7f54b33d202db021
ToniTannoury <tatannoury01@gmail.com>
2023-09-23 02:20:11 +0300

    Initiated user info section

--- frontend/src/components/UserInfoBar.tsx
+++ frontend/src/components/UserInfoBar.tsx
@@ -5,7 +5,15 @@ import Cookies from 'js-cookie';
 import "../stylesheets/infoBar.css";
 
 const UserInfoBar = ({ user, search }: any) => {
+  const dispatch = useDispatch();
+  const { currentUser } = useSelector((state: any) => state.users);
 
+  const [isFollowing, setIsFollowing] = useState(false);
+  const [followerCount, setFollowerCount] = useState(user?.followers?.length || 0);
+  const [isEditingProfilePicture, setIsEditingProfilePicture] = useState(false);
+  const [picture  ,setPicture] = useState<File|string>('')
+  const fileInputRef = useRef<HTMLInputElement>(null);
+  const buttonRef = useRef<HTMLButtonElement>(null);
 
 
 
