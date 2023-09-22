42a82b3821001a1570063241532f1c672954fadd
ToniTannoury <tatannoury01@gmail.com>
2023-09-23 01:45:57 +0300

    Initiated room page

---
+++ frontend/src/app/test/[roomId]/page.tsx
@@ -0,0 +1,26 @@
+'use client'
+import React, { useContext, useEffect, useReducer, useState } from 'react'
+import { useParams, useRouter } from 'next/navigation';
+
+function Room() {
+
+
+
+     
+
+    
+      return result;
+    }
+
+
+  return (
+
+         
+          
+          
+          
+
+</div>
+  )
+}
+export default Room
