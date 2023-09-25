ad7ff828a173e5ea750e102301f0cb2405ba2f67
ToniTannoury <tatannoury01@gmail.com>
2023-09-25 03:38:11 +0300

    Added a second dummy serie to my chart

--- frontend/src/app/adminHome/page.tsx
+++ frontend/src/app/adminHome/page.tsx
@@ -16,6 +16,10 @@ function AdminHome() {
       {
         name: "series-1",
         data: [30, 40, 45, 50, 49, 60, 70, 91]
+      },
+      {
+        name: "series-2",
+        data: [30, 40, 45, 50, 50, 60, 70, 91]
       }
     ]
   })
