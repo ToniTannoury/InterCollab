3e57aaa96c395a6f508f5b300ab54ed69d0f7e4d
ToniTannoury <tatannoury01@gmail.com>
2023-09-25 03:41:14 +0300

    Added line chart state

--- frontend/src/app/adminHome/page.tsx
+++ frontend/src/app/adminHome/page.tsx
@@ -23,6 +23,26 @@ function AdminHome() {
       }
     ]
   })
+  const [lineState , setLineState] = useState<any>({
+    options: {
+      chart: {
+        id: "basic-bar"
+      },
+      xaxis: {
+        categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999]
+      }
+    },
+    series: [
+      {
+        name: "series-1",
+        data: [30, 40, 45, 50, 49, 60, 70, 91]
+      },
+      {
+        name: "series-2",
+        data: [30, 40, 45, 50, 50, 60, 70, 91]
+      }
+    ]
+  })
   const {currentUser} = useSelector((state:any)=>state.users)
   return (
     <div>
