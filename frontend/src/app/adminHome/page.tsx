78bb506448ab47cdadb8160b4d55f226d2c9ab49
ToniTannoury <tatannoury01@gmail.com>
2023-09-25 03:30:53 +0300

    Placed chart state

--- frontend/src/app/adminHome/page.tsx
+++ frontend/src/app/adminHome/page.tsx
@@ -3,7 +3,22 @@ import React, { useState } from 'react'
 import { useSelector } from 'react-redux'
 import Chart from "react-apexcharts";
 function AdminHome() {
-  const [state , setState] = useState<any>()
+  const [state , setState] = useState<any>({
+    options: {
+      chart: {
+        id: "basic-bar"
+      },
+      xaxis: {
+        categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999]
+      }
+    },
+    series: [
+      {
+        name: "series-1",
+        data: [30, 40, 45, 50, 49, 60, 70, 91]
+      }
+    ]
+  })
   const {currentUser} = useSelector((state:any)=>state.users)
   return (
     <div>
