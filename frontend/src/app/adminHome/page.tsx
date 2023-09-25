95e28f1493b275a2c244dfc62d558f7bb3d78106
ToniTannoury <tatannoury01@gmail.com>
2023-09-25 03:34:39 +0300

    Transfered to functional components

--- frontend/src/app/adminHome/page.tsx
+++ frontend/src/app/adminHome/page.tsx
@@ -1,9 +1,9 @@
-import React, { useState } from 'react';
-import { useSelector } from 'react-redux';
+"use client"
+import React, { useState } from 'react'
+import { useSelector } from 'react-redux'
 import Chart from "react-apexcharts";
-
 function AdminHome() {
-  const [state, setState] = useState({
+  const [state , setState] = useState<any>({
     options: {
       chart: {
         id: "basic-bar"
@@ -18,20 +18,18 @@ function AdminHome() {
         data: [30, 40, 45, 50, 49, 60, 70, 91]
       }
     ]
-  });
-
-  const { currentUser } = useSelector((state) => state.users);
-
+  })
+  const {currentUser} = useSelector((state:any)=>state.users)
   return (
     <div>
       <Chart
-        options={state.options}
-        series={state.series}
-        type="bar"
-        width="500"
-      />
+              options={state.options}
+              series={state.series}
+              type="bar"
+              width="500"
+            />
     </div>
-  );
+  )
 }
 
-export default AdminHome;
+export default AdminHome
