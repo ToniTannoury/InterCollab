3a02d6b7a879d14a36d83477f16c1d3c15813265
ToniTannoury <tatannoury01@gmail.com>
2023-09-28 22:52:20 +0300

    Checked maxParticipants

--- backend/controllers/Adminchartscontroller.js
+++ backend/controllers/Adminchartscontroller.js
@@ -3,4 +3,12 @@ const getMostRecentTopRooms = asyncHandler(async (req, res) => {
     // Find the maximum number of participants in any room
     const maxParticipants = await Room.aggregate([
       {
-        $group: {
+        $group: { _id: null,
+          maxParticipants: { $max: '$totalParticipants' },
+        },
+      },
+    ]);
+
+    if (maxParticipants.length === 0) {
+      return res.status(404).json({ message: 'No rooms found' });
+    }
