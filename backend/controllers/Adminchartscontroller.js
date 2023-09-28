e361f726134d5b2c532933355779d3a98db5893a
ToniTannoury <tatannoury01@gmail.com>
2023-09-28 22:53:55 +0300

    sorted and limited most recent top rooms

--- backend/controllers/Adminchartscontroller.js
+++ backend/controllers/Adminchartscontroller.js
@@ -16,4 +16,9 @@ const getMostRecentTopRooms = asyncHandler(async (req, res) => {
 
     const mostRecentTopRooms = await Room.find({
       totalParticipants: maxParticipantCount,
-    })
+    })   .sort({ createdAt: -1 }) // Sort by createdAt in descending order for most recent
+    .limit(10); // You can adjust the limit as needed
+
+  if (mostRecentTopRooms.length === 0) {
+    return res.status(404).json({ message: 'No top rooms found' });
+  }
